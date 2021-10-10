6905f054277c14b09093d3d85cf62a49c188e954
andreapefe <44059755+andreapefe@users.noreply.github.com>
2021-10-10 19:51:18 +0200

    Add files via upload

---
+++ Projet_voilier/Source/main.c
@@ -0,0 +1,16 @@
+/* *************************************************************
+Tire: Projet voilier 2021
+Auteurs: Léa Sheer, Clément Sevillano, Mazin et Andrea Pérez
+Description: Programme principal qui s'éxecute sur le STM32
+***************************************************************** */
+
+//librairies
+#include "stm32f10x.h"
+#include "MyGPIO.h"
+
+//Boucle principale
+int main(void)
+{
+ while(1);
+}
+ 
